937309368930492b1655fd833f38096eaf918c5d
coderwithsense <yeahhpisme@gmail.com>
2023-05-11 17:40:22 +0530

    made the code handle errors

--- ingest.py
+++ ingest.py
@@ -1,22 +1,41 @@
+import sys
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import LlamaCppEmbeddings
-from sys import argv
 
 def main():
-    # Load document and split in chunks
-    loader = TextLoader(argv[1], encoding="utf8")
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    texts = text_splitter.split_documents(documents)
-    # Create embeddings
-    llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
-    # Create and store locally vectorstore
-    persist_directory = 'db'
-    db = Chroma.from_documents(texts, llama, persist_directory=persist_directory)
-    db.persist()
-    db = None
+    # Check if the input file path is provided
+    if len(sys.argv) < 2:
+        print("Please provide the path to the input file as a command line argument.")
+        return
+
+    input_file_path = sys.argv[1]
+
+    try:
+        # Load document and split into chunks
+        loader = TextLoader(input_file_path, encoding="utf8")
+        documents = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        texts = text_splitter.split_documents(documents)
+
+        # Create embeddings
+        llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
+
+        # Create and store vectorstore
+        persist_directory = 'db'
+        db = Chroma.from_documents(texts, llama, persist_directory=persist_directory)
+        db.persist()
+        db = None
+
+        print("Vectorstore created and stored successfully.")
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path and try again.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
--- privateGPT.py
+++ privateGPT.py
@@ -4,36 +4,45 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All
 
-def main():        
-    # Load stored vectorstore
-    llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
-    persist_directory = 'db'
-    db = Chroma(persist_directory=persist_directory, embedding_function=llama)
-    retriever = db.as_retriever()
-    # Prepare the LLM
-    callbacks = [StreamingStdOutCallbackHandler()]
-    llm = GPT4All(model='./models/ggml-gpt4all-j-v1.3-groovy.bin', backend='gptj', callbacks=callbacks, verbose=False)
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-        
-        # Get the answer from the chain
-        res = qa(query)    
-        answer, docs = res['result'], res['source_documents']
+def main():
+    try:
+        # Load stored vectorstore
+        llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
+        persist_directory = 'db'
+        db = Chroma(persist_directory=persist_directory, embedding_function=llama)
+        retriever = db.as_retriever()
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
-        
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        # Prepare the LLM
+        callbacks = [StreamingStdOutCallbackHandler()]
+        llm = GPT4All(model='./models/ggml-gpt4all-j-v1.3-groovy.bin', backend='gptj', callbacks=callbacks, verbose=False)
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
+        # Interactive questions and answers
+        while True:
+            query = input("\nEnter a query: ")
+            if query == "exit":
+                break
+
+            # Get the answer from the chain
+            res = qa(query)
+            answer, docs = res['result'], res['source_documents']
+
+            # Print the result
+            print("\n\n> Question:")
+            print(query)
+            print("\n> Answer:")
+            print(answer)
+
+            # Print the relevant sources used for the answer
+            for document in docs:
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path and try again.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
