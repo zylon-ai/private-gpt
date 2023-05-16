a010910700d1e4e9e2e047e9491be262c0906fbf
Lucas Steuber <78874798+lukeslp@users.noreply.github.com>
2023-05-15 23:06:20 -0700

    FUNCTIONAL w local model

--- ingest.py
+++ ingest.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import openai  # Add this line
+
 from typing import List, Optional
 from dotenv import load_dotenv
 
@@ -10,13 +12,55 @@ from langchain.embeddings import LlamaCppEmbeddings
 from langchain.docstore.document import Document
 from pdfminer.pdfparser import PDFSyntaxError
 from constants import CHROMA_SETTINGS
-
+from dotenv import load_dotenv  # Importing and loading environment variable from .env file
+from langchain.chains import RetrievalQA  # Importing a particular Question-Answer Chain 
+from langchain.embeddings import LlamaCppEmbeddings  # Embedding model to convert input text into vectors.
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  
+from langchain.vectorstores import Chroma  # Vector store that stores text vectors for large-scale retrieval.
+from langchain.llms import GPT4All, LlamaCpp  # Lang Models included here are - LlamaCpp and GPT4All
 
 load_dotenv()
 
+# Initialize OpenAI with API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+
+def generate_response(model: GPT4All, prompt: str, use_openai: bool = False) -> str:
+    """
+    Function to generate a response from a model given a prompt.
+
+    Args:
+        model (GPT4All): The GPT4All model to generate the response.
+        prompt (str): The prompt for the model.
+        use_openai (bool): Whether to use the OpenAI API or a local model. Defaults to False.
+
+    Returns:
+        str: The generated response.
+    """
+    
+    if use_openai:
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=150)
+        return response.choices[0].text.strip()
+    else:
+        # Assumes that model is an instance of a GPT4All model with a 'complete' method
+        return model.complete(prompt, max_tokens=1000)
+
+
 
 def load_single_document(file_path: str) -> Optional[Document]:
-    # Loads a single document from a file path
+    """
+    Function to load a single document from a file path.
+    It checks the extension of the file and uses the appropriate loader to read it. 
+
+    Args: 
+        file_path (str): A string representing the file location on the disk.
+
+    Returns:
+        Optional[Document]: If a document can be loaded, returns an instance of the Document class.
+                            If any error occurs, returns None
+    """
     try:
         if file_path.endswith(".txt"):
             print(f"Loading {file_path} as text...")
@@ -37,7 +81,17 @@ def load_single_document(file_path: str) -> Optional[Document]:
 
 
 def load_documents(source_dir: str) -> List[Document]:
-    # Loads all documents from source documents directory
+    """
+    Function to load all documents from source documents directory
+    It reads all files in the directory with extensions (txt, pdf, csv).
+    It calls the load_single_document function for each file.
+
+    Args:
+        source_dir (str): Directory path containing source documents.
+
+    Returns:
+        List[Document]: If successfully loads documents, returns a list of Document objects.
+    """
     txt_files = glob.glob(os.path.join(source_dir, "**/*.txt"), recursive=True)
     pdf_files = glob.glob(os.path.join(source_dir, "**/*.pdf"), recursive=True)
     csv_files = glob.glob(os.path.join(source_dir, "**/*.csv"), recursive=True)
@@ -48,19 +102,38 @@ def load_documents(source_dir: str) -> List[Document]:
 
 
 def main():
-    # Load environment variables
+    '''
+    Main Function.
+        1. Load environment variables which are required for creating embeddings.
+        2. Load documents from a source directory and split them into chunks.
+        3. Create embeddings for the text chunks
+        4. Store the embeddings in a local database for easy access later on
+    '''
+  # Load environment variables
     persist_directory = os.environ.get('PERSIST_DIRECTORY')
-    source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
-    llama_embeddings_model = os.environ.get('LLAMA_EMBEDDINGS_MODEL')
-    model_n_ctx = os.environ.get('MODEL_N_CTX')
+    source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')   # If SOURCE_DIRECTORY is not set, default to "source_documents".
+    llama_embeddings_model = os.environ.get('LLAMA_EMBEDDINGS_MODEL')    # Load environment variables for GPT4All model
+    model_type = os.getenv('MODEL_TYPE')
+    model_path = os.getenv('MODEL_PATH')
+    model_n_ctx = int(os.getenv('MODEL_N_CTX'))  # Convert this to an integer
+    openai_api_key = os.getenv('OPENAI_API_KEY')
 
-    # Load documents and split in chunks
+    match model_type:
+        case "GPT4All":
+            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', verbose=False)
+
+
+    # Load documents and split in chunks
     print(f"Starting to load documents from {source_directory}")
     documents = load_documents(source_directory)
     print(f"Finished loading documents. Now splitting them into chunks.")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)  # Split the text into 500-token chunks with 50 token overlap
     texts = text_splitter.split_documents(documents)
     print(f"Finished splitting. Total documents: {len(documents)}. Total chunks of text: {len(texts)} (max. 500 tokens each)")
+        # Generate responses and print them
+    for text in texts:
+        response = generate_response(llm, text, use_openai=True)  # Replace llm with your local model
+        print(response)
 
     # Create embeddings
     print(f"Starting to create embeddings...")
@@ -71,9 +144,9 @@ def main():
     print(f"Starting to create and store vectorstore locally...")
     db = Chroma.from_documents(texts, llama, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
     db.persist()
-    db = None
+    db = None   # Set the vectorstore object to None after use to reduce memory footprint
     print(f"Finished creating and storing vectorstore locally. Ingestion process completed successfully.")
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Execute the main function, only if this is the top-level script (not imported). 
--- privateGPT.py
+++ privateGPT.py
@@ -1,57 +1,59 @@
-from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
-from langchain.embeddings import LlamaCppEmbeddings
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from dotenv import load_dotenv  # Importing and loading environment variable from .env file
+from langchain.chains import RetrievalQA  # Importing a particular Question-Answer Chain 
+from langchain.embeddings import LlamaCppEmbeddings  # Embedding model to convert input text into vectors.
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  
+from langchain.vectorstores import Chroma  # Vector store that stores text vectors for large-scale retrieval.
+from langchain.llms import GPT4All, LlamaCpp  # Lang Models included here are - LlamaCpp and GPT4All
 import os
 
-load_dotenv()
+load_dotenv()  # Load environment variables
 
-llama_embeddings_model = os.environ.get("LLAMA_EMBEDDINGS_MODEL")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+llama_embeddings_model = os.environ.get("LLAMA_EMBEDDINGS_MODEL")       # Get path of llama embeddings model
+persist_directory = os.environ.get('PERSIST_DIRECTORY')    # Get the persistence directory to store chains.
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
+model_type = os.environ.get('MODEL_TYPE')         # Get the type of lang chain
+model_path = os.environ.get('MODEL_PATH')         # Get the path of the models (specific to the langchain)
+model_n_ctx = os.environ.get('MODEL_N_CTX')       # The number of context words. It represents how many word tokens will be considered before and after a target token for the model.
 
-from constants import CHROMA_SETTINGS
+from constants import CHROMA_SETTINGS   # Here, chroma setting contains dictionary related to vectorisation technique i.e the number of bits used in hashing.
 
 def main():
-    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
-    db = Chroma(persist_directory=persist_directory, embedding_function=llama, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever()
-    # Prepare the LLM
-    callbacks = [StreamingStdOutCallbackHandler()]
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
-        case _default:
-            print(f"Model {model_type} not supported!")
-            exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    # Interactive questions and answers
+    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)  # Creating an instance of LlamaCppEmbeddings() class for embedding.
+    db = Chroma(persist_directory=persist_directory, embedding_function=llama, client_settings=CHROMA_SETTINGS)  # Storing the vectors (embeddings) in the vector store named Chroma.
+    retriever = db.as_retriever()   # Creating an instance of the retriever to perform text search for question answering.
+
+# Prepare the LLM and Handling Callbacks & Error 
+    callbacks = [StreamingStdOutCallbackHandler()]  # A callback handler that feeds stdout to every call, allowing streaming output from other classes.
+    match model_type:   # Matching lang_model type and executing appropriate steps.
+        case "LlamaCpp":    # If lang_model is LlamapCpp,
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)   # create a new Lang Model instance of LlamaCpp taking model_path and other parameters as arguments
+        case "GPT4All":     # If lang_model is GPT4All,
+            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)  # create a new Lang Model instance of GPT4All taking model path and other parameters as arguments
+        case _default:      # If no valid option - 
+            print(f"Model {model_type} not supported!")         # Print that not supported
+            exit;           # And close the program
+
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)  # Create an instance of Question-Answer Chain based on the available paramters like Lang Model, Retrieval Model etc.
+
+# Interactive questions and answers
     while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
+        query = input("\nEnter a query: ")    # Get the input query 
+        if query == "exit":                   # Exit condition when query is 'exit'
             break
         
-        # Get the answer from the chain
-        res = qa(query)    
-        answer, docs = res['result'], res['source_documents']
+        res = qa(query)     # Query the Question Answer Chain for a response with question as input and storing the result
+        
+        answer, docs = res['result'], res['source_documents']        # Extracting the answer and source documents from result.
 
-        # Print the result
-        print("\n\n> Question:")
+        print("\n\n> Question:")    # Printing the question
         print(query)
         print("\n> Answer:")
-        print(answer)
+        print(answer)               # Printing the answer.
         
-        # Print the relevant sources used for the answer
-        for document in docs:
+        for document in docs:       # For every document attached to the answer ,
             print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+            print(document.page_content)    # Print page content of the relevant documents
+
 
 if __name__ == "__main__":
     main()
