0d24efb42fd27e887009caf7bff432644f4f8a54
MDW <mdeweerd@users.noreply.github.com>
2023-05-16 17:30:44 +0200

    More loaders, generic method

--- src/ingest.py
+++ src/ingest.py
@@ -1,59 +1,95 @@
-import os
 import glob
+import os
 from typing import List
-from dotenv import load_dotenv
 
-from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
+from dotenv import load_dotenv
+from langchain.docstore.document import Document
+from langchain.document_loaders import (  # Docx2txtLoader,
+    CSVLoader,
+    EverNoteLoader,
+    GCSDirectoryLoader,
+    GCSFileLoader,
+    PDFMinerLoader,
+    TextLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredODTLoader,
+    UnstructuredWordDocumentLoader,
+)
+from langchain.embeddings import LlamaCppEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import LlamaCppEmbeddings
-from langchain.docstore.document import Document
+
 from constants import CHROMA_SETTINGS
 
+# Map file extensions to document loaders and their arguments
+LOADER_MAPPING = {
+    ".csv": (CSVLoader, {}),
+    # ".docx": (Docx2txtLoader, {}),
+    ".docx": (UnstructuredWordDocumentLoader, {}),
+    ".enex": (EverNoteLoader, {}),
+    ".gcsdir": (GCSDirectoryLoader, {}),
+    ".gcsfile": (GCSFileLoader, {}),
+    ".html": (UnstructuredHTMLLoader, {}),
+    ".md": (UnstructuredMarkdownLoader, {}),
+    ".odt": (UnstructuredODTLoader, {}),
+    ".pdf": (PDFMinerLoader, {}),
+    ".txt": (TextLoader, {"encoding": "utf8"}),
+    # Add more mappings for other file extensions and loaders as needed
+}
 
 load_dotenv()
 
 
 def load_single_document(file_path: str) -> Document:
-    # Loads a single document from a file path
-    if file_path.endswith(".txt"):
-        loader = TextLoader(file_path, encoding="utf8")
-    elif file_path.endswith(".pdf"):
-        loader = PDFMinerLoader(file_path)
-    elif file_path.endswith(".csv"):
-        loader = CSVLoader(file_path)
-    return loader.load()[0]
+    ext = "." + file_path.rsplit(".", 1)[-1]
+    if ext in LOADER_MAPPING:
+        loader_class, loader_args = LOADER_MAPPING[ext]
+        loader = loader_class(file_path, **loader_args)
+        return loader.load()[0]
+
+    raise ValueError(f"Unsupported file extension '{ext}'")
 
 
 def load_documents(source_dir: str) -> List[Document]:
     # Loads all documents from source documents directory
-    txt_files = glob.glob(os.path.join(source_dir, "**/*.txt"), recursive=True)
-    pdf_files = glob.glob(os.path.join(source_dir, "**/*.pdf"), recursive=True)
-    csv_files = glob.glob(os.path.join(source_dir, "**/*.csv"), recursive=True)
-    all_files = txt_files + pdf_files + csv_files
+    all_files = []
+    for ext in LOADER_MAPPING:
+        all_files.extend(
+            glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True)
+        )
     return [load_single_document(file_path) for file_path in all_files]
 
 
 def main():
-    # Load environment variables
-    persist_directory = os.environ.get('PERSIST_DIRECTORY')
-    source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
-    llama_embeddings_model = os.environ.get('LLAMA_EMBEDDINGS_MODEL')
-    model_n_ctx = os.environ.get('MODEL_N_CTX')
+    # Load environment variables
+    persist_directory = os.environ.get("PERSIST_DIRECTORY")
+    source_directory = os.environ.get("SOURCE_DIRECTORY", "source_documents")
+    llama_embeddings_model = os.environ.get("LLAMA_EMBEDDINGS_MODEL")
+    model_n_ctx = os.environ.get("MODEL_N_CTX")
 
-    # Load documents and split in chunks
+    # Load documents and split in chunks
     print(f"Loading documents from {source_directory}")
     documents = load_documents(source_directory)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=50
+    )
     texts = text_splitter.split_documents(documents)
     print(f"Loaded {len(documents)} documents from {source_directory}")
     print(f"Split into {len(texts)} chunks of text (max. 500 tokens each)")
 
     # Create embeddings
-    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
-    
+    llama = LlamaCppEmbeddings(
+        model_path=llama_embeddings_model, n_ctx=model_n_ctx
+    )
+
     # Create and store locally vectorstore
-    db = Chroma.from_documents(texts, llama, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
+    db = Chroma.from_documents(
+        texts,
+        llama,
+        persist_directory=persist_directory,
+        client_settings=CHROMA_SETTINGS,
+    )
     db.persist()
     db = None
 
