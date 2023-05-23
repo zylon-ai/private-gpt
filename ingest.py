20b93614102b64f934b69777a714e02b40ee97d5
Mike <ytianhui2004@gmail.com>
2023-05-23 14:26:09 +0800

    Update ingest.py
    chunk_size 500 requires too much memory, even 32GB can't fit in it, change to 200, which works fine on 16GB macbook m1

--- ingest.py
+++ ingest.py
@@ -34,7 +34,7 @@ load_dotenv()
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
-chunk_size = 500
+chunk_size = 200
 chunk_overlap = 50
 
 
