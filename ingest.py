7188171b442b159e5034510490d7186fbe61e7c1
Amrith <61175452+amrith-am@users.noreply.github.com>
2023-05-23 17:54:38 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -34,8 +34,8 @@ load_dotenv()
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
-chunk_size = 500
-chunk_overlap = 50
+chunk_size = 2500
+chunk_overlap = 100
 
 
 # Custom document loaders
