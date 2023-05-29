562a71833ccdcf6f9e370ef45fbe0822b168ca60
Daniele <ddanyelus9@gmail.com>
2023-05-29 14:31:08 +0200

    Dynamic N_batch at user's choice

--- privateGPT.py
+++ privateGPT.py
@@ -19,6 +19,7 @@ model_n_ctx = os.environ.get('MODEL_N_CTX')
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 n_gpu_layers = os.environ.get('N_GPU_LAYERS')
 use_mlock = os.environ.get('USE_MLOCK')
+n_batch = os.environ.get('N_BATCH') if os.environ.get('N_BATCH') != None else 512
 
 from constants import CHROMA_SETTINGS
 
@@ -33,7 +34,7 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False,n_gpu_layers=n_gpu_layers, use_mlock=use_mlock,top_p=0.9, n_batch=1024)
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False,n_gpu_layers=n_gpu_layers, use_mlock=use_mlock,top_p=0.9, n_batch=n_batch)
         case "GPT4All":
             llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
         case _default:
