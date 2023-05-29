a3dae71bc7d34d34deb85b77484fce1f74393418
Joseph Mearman <joseph@mearman.co.uk>
2023-05-29 13:52:37 +0100

    Enhance configuration settings with command line arguments
    - Imported argparse in config.py to handle command line arguments.
    - Added new configuration options to be handled by argparse.
    - Configuration variables are now first attempted to be fetched from command line arguments, if not provided then environment variables are used, and if none is provided then defaults are used.
    - Moved argument parsing from privateGPT.py to config.py.
    - Removed parse_arguments function in privateGPT.py, as argument parsing is now handled in config.py.
    - Configuration variables (including flags for hiding source and muting stream) are now fetched directly from the config module in privateGPT.py.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from ctypes import DEFAULT_MODE
+import argparse
 import os
 from dotenv import load_dotenv
 from chromadb.config import Settings
@@ -6,45 +6,109 @@ from chromadb.config import Settings
 load_dotenv()
 
 
+DEFAULT_CHROMA_DB_IMPL = "duckdb+parquet"
 DEFAULT_PERSIST_DIRECTORY = "db"
-PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', DEFAULT_PERSIST_DIRECTORY)
-
-DEFAULT_SOURCE_DIRECTORY = "source_documents"
-SOURCE_DIRECTORY = os.environ.get('SOURCE_DIRECTORY', DEFAULT_SOURCE_DIRECTORY)
-
-DEFAULT_EMBEDDINGS_MODEL_NAME = "all-MiniLM-L6-v2"
-EMBEDDINGS_MODEL_NAME = os.environ.get('EMBEDDINGS_MODEL_NAME',DEFAULT_EMBEDDINGS_MODEL_NAME)
-
-DEFAULT_CHUNK_SIZE = 500
-CHUNK_SIZE = int(os.environ.get('CHUNK_SIZE', DEFAULT_CHUNK_SIZE))
-
-DEFAULT_CHUNK_OVERLAP = 50
-CHUNK_OVERLAP = int(os.environ.get('CHUNK_OVERLAP', DEFAULT_CHUNK_OVERLAP))
-
-DEFAULT_TARGET_SOURCE_CHUNKS = 4
-TARGET_SOURCE_CHUNKS = int(os.environ.get('TARGET_SOURCE_CHUNKS', DEFAULT_TARGET_SOURCE_CHUNKS))
-
-DEFAULT_CHROMA_DB_IMPL = 'duckdb+parquet'
-CHROMA_DB_IMPL = os.environ.get('CHROMA_DB_IMPL', DEFAULT_CHROMA_DB_IMPL)
-
-DEFAULT_CHROMA_TELEMETRY = False
-CHROMA_TELEMETRY = os.environ.get('CHROMA_TELEMETRY', DEFAULT_CHROMA_TELEMETRY)
-
 DEFAULT_CHROMA_PERSIST_DIRECTORY = DEFAULT_PERSIST_DIRECTORY
-CHROMA_PERSIST_DIRECTORY = os.environ.get('CHROMA_PERSIST_DIRECTORY', DEFAULT_CHROMA_PERSIST_DIRECTORY)
+DEFAULT_CHROMA_TELEMETRY = False
+DEFAULT_CHUNK_OVERLAP = 50
+DEFAULT_CHUNK_SIZE = 500
+DEFAULT_EMBEDDINGS_MODEL_NAME = "all-MiniLM-L6-v2"
+DEFAULT_MODEL_N_CTX = 1000
+DEFAULT_MODEL_PATH = "models/ggml-gpt4all-j-v1.3-groovy.bin"
+DEFAULT_MODEL_TYPE = "GPT4All"
+DEFAULT_SOURCE_DIRECTORY = "source_documents"
+DEFAULT_TARGET_SOURCE_CHUNKS = 4
+DEFAULT_MUTE_STREAM = False
+DEFAULT_HIDE_SOURCE_DOCUMENTS = False
 
-CHROMA_SETTINGS = Settings(
-        chroma_db_impl=CHROMA_DB_IMPL,
-        persist_directory=CHROMA_PERSIST_DIRECTORY,
-        anonymized_telemetry=CHROMA_TELEMETRY
+parser = argparse.ArgumentParser(
+    description="privateGPT: Ask questions to your documents without an internet connection, using the power of LLMs."
+)
+parser.add_argument(
+    "--hide-source",
+    "-S",
+    action="store_true",
+    type=bool,
+    default=DEFAULT_HIDE_SOURCE_DOCUMENTS,
+    help="Use this flag to disable printing of source documents used for answers.",
 )
 
-DEFAULT_MODEL_TYPE = "GPT4All"
-MODEL_TYPE = os.environ.get('MODEL_TYPE', DEFAULT_MODEL_TYPE)
+parser.add_argument(
+    "--mute-stream",
+    "-M",
+    type=bool,
+    default=DEFAULT_MUTE_STREAM,
+    action="store_true",
+    help="Use this flag to disable the streaming StdOut callback for LLMs.",
+)
 
-DEFAULT_MODEL_PATH = "models/ggml-gpt4all-j-v1.3-groovy.bin"
-MODEL_PATH = os.environ.get('MODEL_PATH', DEFAULT_MODEL_PATH)
+parser.add_argument("--persist_directory", default="db", help="Persist directory")
+parser.add_argument(
+    "--source_directory", default=DEFAULT_SOURCE_DIRECTORY, help="Source directory"
+)
+parser.add_argument(
+    "--embeddings_model_name",
+    default=DEFAULT_EMBEDDINGS_MODEL_NAME,
+    help="Embeddings model name",
+)
+parser.add_argument(
+    "--chunk_size", type=int, default=DEFAULT_CHUNK_SIZE, help="Chunk size"
+)
+parser.add_argument(
+    "--chunk_overlap", type=int, default=DEFAULT_CHUNK_OVERLAP, help="Chunk overlap"
+)
+parser.add_argument(
+    "--target_source_chunks",
+    type=int,
+    default=DEFAULT_TARGET_SOURCE_CHUNKS,
+    help="Target source chunks",
+)
+parser.add_argument(
+    "--chroma_db_impl", default=DEFAULT_CHROMA_DB_IMPL, help="Chroma DB implementation"
+)
+parser.add_argument(
+    "--chroma_telemetry",
+    type=bool,
+    default=DEFAULT_CHROMA_TELEMETRY,
+    help="Chroma telemetry",
+)
+parser.add_argument(
+    "--chroma_persist_directory",
+    default=DEFAULT_CHROMA_PERSIST_DIRECTORY,
+    help="Chroma persist directory",
+)
+parser.add_argument("--model_type", default=DEFAULT_MODEL_TYPE, help="Model type")
+parser.add_argument(
+    "--model_path", default=DEFAULT_MODEL_PATH, help="Model path"
+)
+parser.add_argument("--model_n_ctx", type=int, default=DEFAULT_MODEL_N_CTX, help="Model n_ctx")
 
-DEFAULT_MODEL_N_CTX = 1000
-MODEL_N_CTX = os.environ.get('MODEL_N_CTX', DEFAULT_MODEL_N_CTX)
+args = parser.parse_args()
 
+PERSIST_DIRECTORY = os.environ.get("PERSIST_DIRECTORY", args.persist_directory)
+SOURCE_DIRECTORY = os.environ.get("SOURCE_DIRECTORY", args.source_directory)
+EMBEDDINGS_MODEL_NAME = os.environ.get(
+    "EMBEDDINGS_MODEL_NAME", args.embeddings_model_name
+)
+CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", args.chunk_size))
+CHUNK_OVERLAP = int(os.environ.get("CHUNK_OVERLAP", args.chunk_overlap))
+TARGET_SOURCE_CHUNKS = int(
+    os.environ.get("TARGET_SOURCE_CHUNKS", args.target_source_chunks)
+)
+CHROMA_DB_IMPL = os.environ.get("CHROMA_DB_IMPL", args.chroma_db_impl)
+CHROMA_TELEMETRY = os.environ.get("CHROMA_TELEMETRY", args.chroma_telemetry)
+CHROMA_PERSIST_DIRECTORY = os.environ.get(
+    "CHROMA_PERSIST_DIRECTORY", args.chroma_persist_directory
+)
+MODEL_TYPE = os.environ.get("MODEL_TYPE", args.model_type)
+MODEL_PATH = os.environ.get("MODEL_PATH", args.model_path)
+MODEL_N_CTX = os.environ.get("MODEL_N_CTX", args.model_n_ctx)
+
+CHROMA_SETTINGS = Settings(
+    chroma_db_impl=CHROMA_DB_IMPL,
+    persist_directory=CHROMA_PERSIST_DIRECTORY,
+    anonymized_telemetry=CHROMA_TELEMETRY,
+)
+
+HIDE_SOURCE_DOCUMENTS = os.environ.get("HIDE_SOURCE_DOCUMENTS", args.hide_source)
+MUTE_STREAM = os.environ.get("MUTE_STREAM", args.mute_stream)
--- privateGPT.py
+++ privateGPT.py
@@ -4,21 +4,19 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
-import argparse
 import config
 
+
 def main():
-    # Parse the command line arguments
-    args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDINGS_MODEL_NAME)
     db = Chroma(
         persist_directory=config.PERSIST_DIRECTORY,
         embedding_function=embeddings,
-        client_settings=config.CHROMA_SETTINGS
+        client_settings=config.CHROMA_SETTINGS,
     )
     retriever = db.as_retriever(search_kwargs={"k": config.TARGET_SOURCE_CHUNKS})
     # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
+    callbacks = [] if config.MUTE_STREAM else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
     match config.MODEL_TYPE:
         case "LlamaCpp":
@@ -26,15 +24,15 @@ def main():
                 model_path=config.MODEL_PATH,
                 n_ctx=config.MODEL_N_CTX,
                 callbacks=callbacks,
-                verbose=False
+                verbose=False,
             )
         case "GPT4All":
             llm = GPT4All(
                 model=config.MODEL_PATH,
                 n_ctx=config.MODEL_N_CTX,
-                backend='gptj',
+                backend="gptj",
                 callbacks=callbacks,
-                verbose=False
+                verbose=False,
             )
         case _default:
             print(f"Model {config.MODEL_TYPE} not supported!")
@@ -43,7 +41,7 @@ def main():
         llm=llm,
         chain_type="stuff",
         retriever=retriever,
-        return_source_documents= not args.hide_source
+        return_source_documents=not config.MUTE_STREAM,
     )
     # Interactive questions and answers
     while True:
@@ -53,7 +51,10 @@ def main():
 
         # Get the answer from the chain
         res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+        answer, docs = (
+            res["result"],
+            [] if config.HIDE_SOURCE_DOCUMENTS else res["source_documents"],
+        )
 
         # Print the result
         print("\n\n> Question:")
@@ -65,18 +66,6 @@ def main():
         for document in docs:
             print("\n> " + document.metadata["source"] + ":")
             print(document.page_content)
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
-
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
-
-    return parser.parse_args()
 
 
 if __name__ == "__main__":
