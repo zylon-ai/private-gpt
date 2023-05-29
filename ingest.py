60d03aada466c1ac7698550176ffa5e26b35ed6a
Joseph Mearman <joseph@mearman.co.uk>
2023-05-29 13:28:04 +0100

    Refactor configuration loading and usage
    - Add a new `config.py` file to centralize the loading of configuration variables from different sources (CLI args, environment variables, and a custom file).
    - Remove the `constants.py` file and update references to use the new `config.py` file.
    - Update `ingest.py`, `privateGPT.py` to use the new configuration loading mechanism.

--- ingest.py
+++ ingest.py
@@ -2,7 +2,6 @@
 import os
 import glob
 from typing import List
-from dotenv import load_dotenv
 from multiprocessing import Pool
 from tqdm import tqdm
 
@@ -24,19 +23,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
-from constants import CHROMA_SETTINGS
 
-
-load_dotenv()
-
-
-# Load environment variables
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
-source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
-embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
-chunk_size = 500
-chunk_overlap = 50
-
+import config
 
 # Custom document loaders
 class MyElmLoader(UnstructuredEmailLoader):
@@ -115,15 +103,15 @@ def process_documents(ignored_files: List[str] = []) -> List[Document]:
     """
     Load documents and split in chunks
     """
-    print(f"Loading documents from {source_directory}")
-    documents = load_documents(source_directory, ignored_files)
+    print(f"Loading documents from {config.SOURCE_DIRECTORY}")
+    documents = load_documents(config.SOURCE_DIRECTORY, ignored_files)
     if not documents:
         print("No new documents to load")
         exit(0)
-    print(f"Loaded {len(documents)} new documents from {source_directory}")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    print(f"Loaded {len(documents)} new documents from {config.SOURCE_DIRECTORY}")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=config.CHUNK_SIZE, chunk_overlap=config.CHUNK_OVERLAP)
     texts = text_splitter.split_documents(documents)
-    print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
+    print(f"Split into {len(texts)} chunks of text (max. {config.CHUNK_SIZE} tokens each)")
     return texts
 
 def does_vectorstore_exist(persist_directory: str) -> bool:
@@ -141,12 +129,16 @@ def does_vectorstore_exist(persist_directory: str) -> bool:
 
 def main():
     # Create embeddings
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDINGS_MODEL_NAME)
 
-    if does_vectorstore_exist(persist_directory):
+    if does_vectorstore_exist(config.PERSIST_DIRECTORY):
         # Update and store locally vectorstore
-        print(f"Appending to existing vectorstore at {persist_directory}")
-        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+        print(f"Appending to existing vectorstore at {config.PERSIST_DIRECTORY}")
+        db = Chroma(
+            persist_directory=config.PERSIST_DIRECTORY,
+            embedding_function=embeddings,
+            client_settings=config.CHROMA_SETTINGS
+        )
         collection = db.get()
         texts = process_documents([metadata['source'] for metadata in collection['metadatas']])
         print(f"Creating embeddings. May take some minutes...")
@@ -156,7 +148,12 @@ def main():
         print("Creating new vectorstore")
         texts = process_documents()
         print(f"Creating embeddings. May take some minutes...")
-        db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
+        db = Chroma.from_documents(
+            texts,
+            embeddings,
+            persist_directory=config.PERSIST_DIRECTORY,
+            client_settings=config.CHROMA_SETTINGS
+        )
     db.persist()
     db = None
 
--- privateGPT.py
+++ privateGPT.py
@@ -1,43 +1,50 @@
 #!/usr/bin/env python3
-from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
-import os
 import argparse
-
-load_dotenv()
-
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
-
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
-
-from constants import CHROMA_SETTINGS
+import config
 
 def main():
     # Parse the command line arguments
     args = parse_arguments()
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+    embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDINGS_MODEL_NAME)
+    db = Chroma(
+        persist_directory=config.PERSIST_DIRECTORY,
+        embedding_function=embeddings,
+        client_settings=config.CHROMA_SETTINGS
+    )
+    retriever = db.as_retriever(search_kwargs={"k": config.TARGET_SOURCE_CHUNKS})
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
-    match model_type:
+    match config.MODEL_TYPE:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(
+                model_path=config.MODEL_PATH,
+                n_ctx=config.MODEL_N_CTX,
+                callbacks=callbacks,
+                verbose=False
+            )
         case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+            llm = GPT4All(
+                model=config.MODEL_PATH,
+                n_ctx=config.MODEL_N_CTX,
+                backend='gptj',
+                callbacks=callbacks,
+                verbose=False
+            )
         case _default:
-            print(f"Model {model_type} not supported!")
-            exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+            print(f"Model {config.MODEL_TYPE} not supported!")
+            exit(1)
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents= not args.hide_source
+    )
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
