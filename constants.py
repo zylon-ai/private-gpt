9ece4e7410c14621b791397bfcc5117e6fe094c7
n <nurterzi1925@icloud.com>
2023-06-10 13:22:15 +0300

    Update constants.py
    enjoy

--- constants.py
+++ constants.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
 from chromadb.config import Settings
+import json
 
 load_dotenv()
+
+try:
+    load_dotenv()
+except Exception as e:
+    print("Error loading .env file:", str(e))
+
+# PERSIST_DIRECTORY erorr control
+PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
+if not PERSIST_DIRECTORY:
+    print("PERSIST_DIRECTORY is not defined in the environment.")
 
 # Define the folder for storing database
 PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
@@ -12,4 +23,18 @@ CHROMA_SETTINGS = Settings(
         chroma_db_impl='duckdb+parquet',
         persist_directory=PERSIST_DIRECTORY,
         anonymized_telemetry=False
+)
+
+
+with open('settings.json') as f:
+    settings = json.load(f)
+
+chroma_db_impl = settings.get('chroma_db_impl')
+persist_directory = settings.get('persist_directory')
+anonymized_telemetry = settings.get('anonymized_telemetry')
+
+CHROMA_SETTINGS = Settings(
+    chroma_db_impl=chroma_db_impl,
+    persist_directory=persist_directory,
+    anonymized_telemetry=anonymized_telemetry
 )
