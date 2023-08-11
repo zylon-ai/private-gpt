d34ea9af121a7906f910464cf2dc465c02ff29ee
Luca Tumedei <luca@theaveragedev.com>
2023-08-11 14:02:32 +0200

    control llm n_threads with env file, support STDIN pipe and query arg

--- privateGPT.py
+++ privateGPT.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import sys
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
+from langchain.vectorstores.base import VectorStoreRetriever
 from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms.base import LLM
 import os
 import argparse
 import time
@@ -18,9 +21,32 @@ model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
+model_n_threads = int(os.environ.get('MODEL_N_THREADS',4))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
+
+def answer_query(query: str, hide_source: bool, retriever: VectorStoreRetriever,llm: LLM ) -> None:
+    if query.strip() == "":
+        return
+
+    # Get the answer from the chain
+    start = time.time()
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not hide_source)
+    res = qa(query)
+    answer, docs = res['result'], [] if hide_source else res['source_documents']
+    end = time.time()
+
+    # Print the result
+    print("\n\n> Question:")
+    print(query)
+    print(f"\n> Answer (took {round(end - start, 2)} s.):")
+    print(answer)
+
+    # Print the relevant sources used for the answer
+    for document in docs:
+        print("\n> " + document.metadata["source"] + ":")
+        print(document.page_content)
 
 def main():
     # Parse the command line arguments
@@ -33,38 +59,33 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False, n_threads=model_n_threads)
         case "GPT4All":
-            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False, n_threads=model_n_threads)
         case _default:
             # raise exception if model_type is not supported
             raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
-        
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+
+    hide_source = args.hide_source
+
+    # If there is a query in the STDIN, answer it and exit.
+    if not sys.stdin.isatty():
+        query = sys.stdin.read()
+        answer_query(query, hide_source, retriever, llm)
+        sys.exit(0)
+
+    # If there is a query in the command line arguments, answer it and exit.
+    if args.query:
+        answer_query(args.query, hide_source, retriever, llm)
+        sys.exit(0)
+
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
         if query == "exit":
             break
-        if query.strip() == "":
-            continue
 
-        # Get the answer from the chain
-        start = time.time()
-        res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-        end = time.time()
-
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        print(answer)
-
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        answer_query(query, hide_source, retriever, llm)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
@@ -75,6 +96,9 @@ def parse_arguments():
     parser.add_argument("--mute-stream", "-M",
                         action='store_true',
                         help='Use this flag to disable the streaming StdOut callback for LLMs.')
+    
+    parser.add_argument('query', nargs='?', default=None,
+                        help='The question to ask to your documents.')
 
     return parser.parse_args()
 
