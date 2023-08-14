3e3187fd6d5dee9a154f1dcd04cf3e9cb10b4b3c
José Afonso Guerra-Assunção <a.guerra@ucl.ac.uk>
2023-08-14 09:41:38 +0100

    Updating code for personal preference adding sentence_transformer to requirements and adding parameters for gpu and threads

--- privateGPT.py
+++ privateGPT.py
@@ -17,6 +17,8 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
+model_n_gpu_layers = os.environ.get('MODEL_N_GPU_LAYERS')
+model_n_threads = os.environ.get('MODEL_N_THREADS')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
@@ -33,7 +35,7 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False, n_gpu_layers=model_n_gpu_layers, n_threads=model_n_threads)
         case "GPT4All":
             llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
