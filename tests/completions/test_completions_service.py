6869392e9f90cbc69ae5e0dedde09d2034dd30d8
Pablo Orgaz <pablo@Pablos-MacBook-Pro.local>
2023-10-01 18:55:01 +0200

    Fix tests

--- tests/completions/test_completions_service.py
+++ tests/completions/test_completions_service.py
@@ -4,6 +4,6 @@ from tests.fixtures.mock_injector import MockInjector
 
 def test_completions_service_produces_a_stream(injector: MockInjector) -> None:
     service = injector.get(CompletionsService)
-    response = service.stream_complete("test", model="mock")
+    response = service.stream_complete("test", model_name="mock")
     text = "".join([r.delta or "" for r in response])
     assert text == "test"
