fb96e541c7d45ee969fa9e57ead7f53c21bfef72
Pablo Orgaz <pablo@Pablos-MacBook-Pro.local>
2023-10-03 13:18:17 +0200

    Add integration test

--- tests/llm/test_llm_service.py
+++ tests/llm/test_llm_service.py
@@ -1,3 +1,6 @@
+import openai.openai_response
+from fastapi.testclient import TestClient
+from openai.api_requestor import parse_stream
 
 from private_gpt.llm.llm_service import LLMService
 from tests.fixtures.mock_injector import MockInjector
@@ -17,3 +20,16 @@ async def test_llm_service_chat_produces_a_stream(
     stream = await service.stream_chat("test")
     response = "".join([response.delta or "" async for response in stream])
     assert response == "user: test\nassistant: "
+
+
+async def test_llm_endpoint_produces_sse_stream(test_client: TestClient) -> None:
+    response = test_client.get("/completions?prompt=test")
+
+    raw_events = response.text.split("\n\n")
+    events = [
+        item.removeprefix("data: ") for item in raw_events if item.startswith("data: ")
+    ]
+    assert response.status_code == 200
+    assert "text/event-stream" in response.headers["content-type"]
+    assert len(events) > 0
+    assert events[-1] == "[DONE]"
