3565eb5363e79cb0a3d5afe17a2fd340dc7e1001
Iván Martínez <ivanmartit@gmail.com>
2023-10-03 22:38:29 +0200

    Make retrieval API a POST

--- private_gpt/retrieval/routes.py
+++ private_gpt/retrieval/routes.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass
+
 from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 from private_gpt.di import root_injector
 from private_gpt.retrieval.retrieval_service import RetrievalService
@@ -8,8 +11,17 @@ from private_gpt.retrieval.retrieval_service import RetrievalService
 retrieval_router = APIRouter()
 
 
-@retrieval_router.get("/retrieve")
-async def retrieve(query: str, limit: int = 10, context_size: int = 0) -> JSONResponse:
+@dataclass
+class RetrieveBody(BaseModel):
+    query: str
+    limit: int = 10
+    context_size: int = 0
+
+
+@retrieval_router.post("/retrieve")
+async def retrieve(body: RetrieveBody) -> JSONResponse:
     service = root_injector.get(RetrievalService)
-    results = await service.retrieve_relevant_nodes(query, limit, context_size)
+    results = await service.retrieve_relevant_nodes(
+        body.query, body.limit, body.context_size
+    )
     return JSONResponse(content=jsonable_encoder(results))
