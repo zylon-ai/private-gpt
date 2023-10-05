169e11639c76f418785c5cc060e83aecb4ed40f1
Pablo Orgaz <pablo@Pablos-MacBook-Pro.local>
2023-10-05 19:47:12 +0200

    Typo in context_files.py

--- private_gpt/open_ai/extensions/context_files.py
+++ private_gpt/open_ai/extensions/context_files.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class ContextFiles:
-    collection: list[str] | None = None
+    collections: list[str] | None = None
     ids: list[str] | None = None
