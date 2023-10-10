019a98045a8646a3f2115c7e0d23f8226f315c2c
Iván Martínez <ivanmartit@gmail.com>
2023-10-10 19:09:13 +0200

    Rename ingest routes to ingest_router.py

--- private_gpt/main.py
+++ private_gpt/main.py
@@ -10,7 +10,7 @@ from private_gpt.server.chat.chat_router import chat_router
 from private_gpt.server.chunks.chunks_router import chunks_router
 from private_gpt.server.completions.completions_router import completions_router
 from private_gpt.server.embeddings.embeddings_router import embeddings_router
-from private_gpt.server.ingest.routes import ingest_router
+from private_gpt.server.ingest.ingest_router import ingest_router
 from private_gpt.settings.settings import settings
 
 # Remove pre-configured logging handler
