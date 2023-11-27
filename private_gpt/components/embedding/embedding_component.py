34e5b3d6208baadb155d0da1566187fe9eef1244
Ibraheem Akbar <121730444+Akbarable@users.noreply.github.com>
2023-11-27 15:15:33 +0500

    Update embedding_component.py

--- private_gpt/components/embedding/embedding_component.py
+++ private_gpt/components/embedding/embedding_component.py
@@ -16,31 +16,39 @@ class EmbeddingComponent:
 
     @inject
     def __init__(self, settings: Settings) -> None:
-        embedding_mode = settings.embedding.mode
-        logger.info("Initializing the embedding model in mode=%s", embedding_mode)
+        embedding_mode = settings.chatbot.embedding_mode  
+        chatbot_mode = settings.chatbot.mode
+
+        if chatbot_mode == "hybrid":
+            embedding_mode = settings.chatbot.embedding_mode
+
         match embedding_mode:
             case "local":
-                from llama_index.embeddings import HuggingFaceEmbedding
-
-                self.embedding_model = HuggingFaceEmbedding(
-                    model_name=settings.local.embedding_hf_model_name,
-                    cache_folder=str(models_cache_path),
-                )
+                self.embedding_model = self._create_local_embedding(settings)
             case "sagemaker":
-
-                from private_gpt.components.embedding.custom.sagemaker import (
-                    SagemakerEmbedding,
-                )
-
-                self.embedding_model = SagemakerEmbedding(
-                    endpoint_name=settings.sagemaker.embedding_endpoint_name,
-                )
+                self.embedding_model = self._create_sagemaker_embedding(settings)
             case "openai":
-                from llama_index import OpenAIEmbedding
-
-                openai_settings = settings.openai.api_key
-                self.embedding_model = OpenAIEmbedding(api_key=openai_settings)
+                self.embedding_model = self._create_openai_embedding(settings)
             case "mock":
-                # Not a random number, is the dimensionality used by
-                # the default embedding model
                 self.embedding_model = MockEmbedding(384)
+            case _:
+                raise ValueError(f"Unsupported embedding mode chatbot: {chatbot_mode}") 
+
+    def _create_local_embedding(self, settings: Settings) -> BaseEmbedding:
+        from llama_index.embeddings import HuggingFaceEmbedding
+        return HuggingFaceEmbedding(
+            model_name=settings.local.embedding_hf_model_name,
+            cache_folder=str(models_cache_path),
+        )
+
+    def _create_sagemaker_embedding(self, settings: Settings) -> BaseEmbedding:
+        from private_gpt.components.embedding.custom.sagemaker import SagemakerEmbedding
+        return SagemakerEmbedding(
+            endpoint_name=settings.sagemaker.embedding_endpoint_name,
+        )
+
+    def _create_openai_embedding(self, settings: Settings) -> BaseEmbedding:
+        from llama_index import OpenAIEmbedding
+        openai_settings = settings.openai.api_key
+        return OpenAIEmbedding(api_key=openai_settings)
+
