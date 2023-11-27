9197c37d49640eb1ef4c82836d64e5b0034d98c2
Ibraheem Akbar <121730444+Akbarable@users.noreply.github.com>
2023-11-27 15:18:00 +0500

    Update llm_component.py

--- private_gpt/components/llm/llm_component.py
+++ private_gpt/components/llm/llm_component.py
@@ -3,13 +3,19 @@ import logging
 from injector import inject, singleton
 from llama_index.llms import MockLLM
 from llama_index.llms.base import LLM
+from llama_index.llms.llama_utils import completion_to_prompt, messages_to_prompt
+from llama_index.llms import LlamaCPP
 
-from private_gpt.components.llm.prompt_helper import get_prompt_style
 from private_gpt.paths import models_path
 from private_gpt.settings.settings import Settings
 
+
 logger = logging.getLogger(__name__)
 
+
+
+
+# Inside LLMComponent class
 
 @singleton
 class LLMComponent:
@@ -17,43 +23,43 @@ class LLMComponent:
 
     @inject
     def __init__(self, settings: Settings) -> None:
-        llm_mode = settings.llm.mode
-        logger.info("Initializing the LLM in mode=%s", llm_mode)
-        match settings.llm.mode:
+        llm_mode = settings.chatbot.llm_mode  # Move this line outside the if block
+        chatbot_mode = settings.chatbot.mode
+
+        if chatbot_mode == "hybrid":
+            llm_mode = settings.chatbot.llm_mode
+            #embedding_mode = settings.chatbot.embedding_mode
+
+        match llm_mode:
             case "local":
-                from llama_index.llms import LlamaCPP
-
-                prompt_style_cls = get_prompt_style(settings.local.prompt_style)
-                prompt_style = prompt_style_cls(
-                    default_system_prompt=settings.local.default_system_prompt
-                )
-
-                self.llm = LlamaCPP(
-                    model_path=str(models_path / settings.local.llm_hf_model_file),
-                    temperature=0.1,
-                    max_new_tokens=settings.llm.max_new_tokens,
-                    # llama2 has a context window of 4096 tokens,
-                    # but we set it lower to allow for some wiggle room
-                    context_window=3900,
-                    generate_kwargs={},
-                    # All to GPU
-                    model_kwargs={"n_gpu_layers": -1},
-                    # transform inputs into Llama2 format
-                    messages_to_prompt=prompt_style.messages_to_prompt,
-                    completion_to_prompt=prompt_style.completion_to_prompt,
-                    verbose=True,
-                )
-
+                self.llm = self._create_local_llm(settings)
             case "sagemaker":
-                from private_gpt.components.llm.custom.sagemaker import SagemakerLLM
-
-                self.llm = SagemakerLLM(
-                    endpoint_name=settings.sagemaker.llm_endpoint_name,
-                )
+                self.llm = self._create_sagemaker_llm(settings)
             case "openai":
-                from llama_index.llms import OpenAI
-
-                openai_settings = settings.openai.api_key
-                self.llm = OpenAI(api_key=openai_settings)
+                self.llm = self._create_openai_llm(settings)
             case "mock":
                 self.llm = MockLLM()
+            case _:
+                raise ValueError(f"Unsupported llm mode: {llm_mode}")
+
+    def _create_local_llm(self, settings: Settings) -> LLM:
+        return LlamaCPP(
+            model_path=str(models_path / settings.local.llm_hf_model_file),
+            temperature=0.1,
+            context_window=3900,
+            generate_kwargs={},
+            model_kwargs={"n_gpu_layers": -1},
+            messages_to_prompt=messages_to_prompt,
+            completion_to_prompt=completion_to_prompt,
+            verbose=True,
+        )
+
+    def _create_sagemaker_llm(self, settings: Settings) -> LLM:
+        from private_gpt.components.llm.custom.sagemaker import SagemakerLLM
+        return SagemakerLLM(endpoint_name=settings.sagemaker.llm_endpoint_name)
+
+    def _create_openai_llm(self, settings: Settings) -> LLM:
+        from llama_index.llms import OpenAI
+        openai_settings = settings.openai.api_key
+        return OpenAI(api_key=openai_settings)
+
