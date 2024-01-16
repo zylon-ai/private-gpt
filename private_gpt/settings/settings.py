0ee082ff6e2f74b24a75fa4e9f25a67728d8ed56
CognitiveTech <cognitivetechniq@yahoo.com>
2024-01-16 15:16:32 -0500

    chatml to settings.py

--- private_gpt/settings/settings.py
+++ private_gpt/settings/settings.py
@@ -98,7 +98,7 @@ class LocalSettings(BaseModel):
     embedding_hf_model_name: str = Field(
         description="Name of the HuggingFace model to use for embeddings"
     )
-    prompt_style: Literal["default", "llama2", "tag", "mistral"] = Field(
+    prompt_style: Literal["default", "llama2", "tag", "mistral", "chatml"] = Field(
         "llama2",
         description=(
             "The prompt style to use for the chat engine. "
