01620a7926df426421f4bd13470a3f77018cc039
jeff <jeffreyr@securitydept.com>
2024-02-22 17:27:38 -0500

    patch

--- private_gpt/components/node_store/node_store_component.py
+++ private_gpt/components/node_store/node_store_component.py
@@ -2,12 +2,10 @@ import logging
 
 from injector import inject, singleton
 from llama_index.storage.docstore import BaseDocumentStore, SimpleDocumentStore
-from llama_index.storage.docstore import MongoDocumentStore
-from llama_index.storage.index_store import SimpleIndexStore, MongoIndexStore, RedisIndexStore
+from llama_index.storage.index_store import SimpleIndexStore
 from llama_index.storage.index_store.types import BaseIndexStore
 
 from private_gpt.paths import local_data_path
-from private_gpt.settings.settings import Settings
 
 logger = logging.getLogger(__name__)
 
@@ -18,26 +16,11 @@ class NodeStoreComponent:
     doc_store: BaseDocumentStore
 
     @inject
-    def __init__(self, settings: Settings) -> None:
-
+    def __init__(self) -> None:
         try:
-
-            if settings.indexstore.mode == "mongo":
-                self.index_store = MongoIndexStore.from_host_and_port(
-                        host=settings.indexstore.host,
-                        port=settings.indexstore.port
-                    )
-            elif settings.indexstore.mode == "redis":
-                self.index_store = RedisIndexStore.from_host_and_port(
-                        host=settings.indexstore.host,
-                        port=settings.indexstore.port,
-                        namespace="llama"
-                    )
-            else:
-                self.index_store = SimpleIndexStore.from_persist_dir(
-                    persist_dir=str(local_data_path)
-                )
-
+            self.index_store = SimpleIndexStore.from_persist_dir(
+                persist_dir=str(local_data_path)
+            )
         except FileNotFoundError:
             logger.debug("Local index store not found, creating a new one")
             self.index_store = SimpleIndexStore()
