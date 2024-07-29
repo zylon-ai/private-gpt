2e0fb90f7329605082375655d47958f353f4a330
Javier Martinez <javiermartinezalvarez98@gmail.com>
2024-07-29 13:25:42 +0200

    feat(llm): autopull ollama models (#2019)
    * chore: update ollama (llm)
    
    * feat: allow to autopull ollama models
    
    * fix: mypy
    
    * chore: install always ollama client
    
    * refactor: check connection and pull ollama method to utils
    
    * docs: update ollama config with autopulling info
    
    ...

--- private_gpt/components/embedding/embedding_component.py
+++ private_gpt/components/embedding/embedding_component.py
@@ -71,16 +71,46 @@ class EmbeddingComponent:
                     from llama_index.embeddings.ollama import (  # type: ignore
                         OllamaEmbedding,
                     )
+                    from ollama import Client  # type: ignore
                 except ImportError as e:
                     raise ImportError(
                         "Local dependencies not found, install with `poetry install --extras embeddings-ollama`"
                     ) from e
 
                 ollama_settings = settings.ollama
+
+                # Calculate embedding model. If not provided tag, it will be use latest
+                model_name = (
+                    ollama_settings.embedding_model + ":latest"
+                    if ":" not in ollama_settings.embedding_model
+                    else ollama_settings.embedding_model
+                )
+
                 self.embedding_model = OllamaEmbedding(
-                    model_name=ollama_settings.embedding_model,
+                    model_name=model_name,
                     base_url=ollama_settings.embedding_api_base,
                 )
+
+                if ollama_settings.autopull_models:
+                    if ollama_settings.autopull_models:
+                        from private_gpt.utils.ollama import (
+                            check_connection,
+                            pull_model,
+                        )
+
+                        # TODO: Reuse llama-index client when llama-index is updated
+                        client = Client(
+                            host=ollama_settings.embedding_api_base,
+                            timeout=ollama_settings.request_timeout,
+                        )
+
+                        if not check_connection(client):
+                            raise ValueError(
+                                f"Failed to connect to Ollama, "
+                                f"check if Ollama server is running on {ollama_settings.api_base}"
+                            )
+                        pull_model(client, model_name)
+
             case "azopenai":
                 try:
                     from llama_index.embeddings.azure_openai import (  # type: ignore
